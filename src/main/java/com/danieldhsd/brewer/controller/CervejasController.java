a8d3ca49acc1038f37e39036781f2886fe48dc94
Daniel Dutra <danieldhsd@gmail.com>
2021-01-30 13:15:00 -0300

    Forwad vs Redirect

--- src/main/java/com/danieldhsd/brewer/controller/CervejasController.java
+++ src/main/java/com/danieldhsd/brewer/controller/CervejasController.java
@@ -3,9 +3,11 @@ package com.danieldhsd.brewer.controller;
 import javax.validation.Valid;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import com.danieldhsd.brewer.model.Cerveja;
 
@@ -18,12 +20,14 @@ public class CervejasController {
 	}
 	
 	@RequestMapping(value = "/cervejas/novo", method = RequestMethod.POST)
-	public String cadastrar(@Valid Cerveja cerveja, BindingResult result) {
+	public String cadastrar(@Valid Cerveja cerveja, BindingResult result, Model model,
+			RedirectAttributes attributes) {
 		if(result.hasErrors()) {
-			System.out.println("Tem erros");
+			model.addAttribute("mensagem", "Erro no formulário");
+			return "cerveja/CadastroCerveja";
 		}
 		
-		System.out.println("SKU: " + cerveja.getSku());
-		return "cerveja/CadastroCerveja";
+		attributes.addFlashAttribute("mensagem", "Cerveja salva com sucesso!");
+		return "redirect:/cervejas/novo";
 	}
 }
