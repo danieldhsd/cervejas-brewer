8bb31ca6acb41439edc8a31193e9e380f50d2dd2
Daniel Dutra <danieldhsd@gmail.com>
2021-02-09 21:33:59 -0300

    Implementando o cadastro e validacao de estilos

---
+++ src/main/java/com/danieldhsd/brewer/service/CadastroEstiloService.java
@@ -0,0 +1,20 @@
+package com.danieldhsd.brewer.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.danieldhsd.brewer.model.Estilo;
+import com.danieldhsd.brewer.repository.Estilos;
+
+@Service
+public class CadastroEstiloService {
+
+	@Autowired
+	private Estilos estilos;
+	
+	@Transactional
+	public void salvar(Estilo estilo) {
+		estilos.save(estilo);
+	}
+}
