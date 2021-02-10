0ee911b3ac9894b6c28c49aa321ebc49f3bd0d40
Daniel Dutra <danieldhsd@gmail.com>
2021-02-09 22:26:02 -0300

    Validando se ja existe um estilo com o mesmo nome persistido

--- src/main/java/com/danieldhsd/brewer/service/CadastroEstiloService.java
+++ src/main/java/com/danieldhsd/brewer/service/CadastroEstiloService.java
@@ -1,11 +1,14 @@
 package com.danieldhsd.brewer.service;
 
+import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import com.danieldhsd.brewer.model.Estilo;
 import com.danieldhsd.brewer.repository.Estilos;
+import com.danieldhsd.brewer.service.exception.NomeEstiloJaCadastradoException;
 
 @Service
 public class CadastroEstiloService {
@@ -15,6 +18,12 @@ public class CadastroEstiloService {
 	
 	@Transactional
 	public void salvar(Estilo estilo) {
+		Optional<Estilo> estiloOptional = estilos.findByNomeIgnoreCase(estilo.getNome());
+		
+		if(estiloOptional.isPresent()) {
+			throw new NomeEstiloJaCadastradoException("Nome do estilo já cadastrado");
+		}
+		
 		estilos.save(estilo);
 	}
 }
