ef6a4e7e5ffdb7902bc3e992ea0f230872a76ca0
Daniel Dutra <danieldhsd@gmail.com>
2021-02-24 19:34:30 -0300

    Criando pastas para salvar as fotos

--- src/main/java/com/danieldhsd/brewer/config/ServiceConfig.java
+++ src/main/java/com/danieldhsd/brewer/config/ServiceConfig.java
@@ -1,12 +1,19 @@
 package com.danieldhsd.brewer.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
 import com.danieldhsd.brewer.service.CadastroCervejaService;
+import com.danieldhsd.brewer.storage.FotoStorage;
+import com.danieldhsd.brewer.storage.local.FotoStorageLocal;
 
 @Configuration
 @ComponentScan(basePackageClasses = CadastroCervejaService.class)
 public class ServiceConfig {
 
+	@Bean
+	public FotoStorage fotoStorage() {
+		return new FotoStorageLocal();
+	}
 }
