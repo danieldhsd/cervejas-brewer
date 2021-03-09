bca08a3fced01678206eb97e08f52b7f35dad97d
Daniel Dutra <danieldhsd@gmail.com>
2021-03-09 20:36:02 -0300

    Continuando implementacao da paginacao na tela de pesquisa de cervejas

--- src/main/java/com/danieldhsd/brewer/repository/helper/cerveja/CervejasQueries.java
+++ src/main/java/com/danieldhsd/brewer/repository/helper/cerveja/CervejasQueries.java
@@ -2,10 +2,12 @@ package com.danieldhsd.brewer.repository.helper.cerveja;
 
 import java.util.List;
 
+import org.springframework.data.domain.Pageable;
+
 import com.danieldhsd.brewer.model.Cerveja;
 import com.danieldhsd.brewer.repository.filter.CervejaFilter;
 
 public interface CervejasQueries {
 	
-	public List<Cerveja> filtrar(CervejaFilter filtro);
+	public List<Cerveja> filtrar(CervejaFilter filtro, Pageable pageable);
 }
