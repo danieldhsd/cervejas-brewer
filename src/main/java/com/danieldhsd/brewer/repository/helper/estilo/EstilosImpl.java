c0bbf4887df4b3ef4a86f82b365b66ff113dfa1b
Daniel Dutra <danieldhsd@gmail.com>
2021-03-11 21:39:36 -0300

    Criando beanUtil para paginacao

--- src/main/java/com/danieldhsd/brewer/repository/helper/estilo/EstilosImpl.java
+++ src/main/java/com/danieldhsd/brewer/repository/helper/estilo/EstilosImpl.java
@@ -9,6 +9,7 @@ import org.hibernate.criterion.MatchMode;
 import org.hibernate.criterion.Order;
 import org.hibernate.criterion.Projections;
 import org.hibernate.criterion.Restrictions;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageImpl;
 import org.springframework.data.domain.Pageable;
@@ -18,11 +19,16 @@ import org.springframework.util.StringUtils;
 
 import com.danieldhsd.brewer.model.Estilo;
 import com.danieldhsd.brewer.repository.filter.EstiloFilter;
+import com.danieldhsd.brewer.repository.paginacao.PaginacaoUtil;
 
 public class EstilosImpl implements EstilosQueries {
 
 	@PersistenceContext
 	private EntityManager manager;
+	
+	@Autowired
+	private PaginacaoUtil paginacaoUtil;
+
 
 	@SuppressWarnings("unchecked")
 	@Override
@@ -30,19 +36,7 @@ public class EstilosImpl implements EstilosQueries {
 	public Page<Estilo> filtrar(EstiloFilter filtro, Pageable pageable) {
 		Criteria criteria = manager.unwrap(Session.class).createCriteria(Estilo.class);
 		
-		int paginaAtual = pageable.getPageNumber();
-		int totalRegistrosPorPagina = pageable.getPageSize();
-		int primeiroRegistro = paginaAtual * totalRegistrosPorPagina;
-		
-		criteria.setFirstResult(primeiroRegistro);
-		criteria.setMaxResults(totalRegistrosPorPagina);
-		
-		Sort sort = pageable.getSort();
-		if (sort != null) {
-			Sort.Order order = sort.iterator().next();
-			String property = order.getProperty();
-			criteria.addOrder(order.isAscending() ? Order.asc(property) : Order.desc(property));
-		}
+		paginacaoUtil.preparar(criteria, pageable);
 		
 		adicionarFiltro(filtro, criteria);
 		
