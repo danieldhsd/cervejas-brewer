73d98f8a134fb680ad83ffe9dc4f6ca9b0e2f360
Daniel Dutra <danieldhsd@gmail.com>
2021-03-26 20:48:50 -0300

    Finalizando o cadastro de cidades

--- src/main/java/com/danieldhsd/brewer/controller/CidadesController.java
+++ src/main/java/com/danieldhsd/brewer/controller/CidadesController.java
@@ -2,15 +2,24 @@ package com.danieldhsd.brewer.controller;
 
 import java.util.List;
 
+import javax.validation.Valid;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import com.danieldhsd.brewer.model.Cidade;
 import com.danieldhsd.brewer.repository.Cidades;
+import com.danieldhsd.brewer.repository.Estados;
+import com.danieldhsd.brewer.service.CadastroCidadeService;
+import com.danieldhsd.brewer.service.exception.NomeCidadeJaCadastradaException;
 
 @Controller
 @RequestMapping("/cidades")
@@ -18,10 +27,18 @@ public class CidadesController {
 	
 	@Autowired
 	private Cidades cidades;
+	
+	@Autowired
+	private Estados estados;
+	
+	@Autowired
+	private CadastroCidadeService cadastroCidadeService;
 
 	@RequestMapping("/nova")
-	public String nova() {
-		return "cidade/CadastroCidade";
+	public ModelAndView nova(Cidade cidade) {
+		ModelAndView mv = new ModelAndView("cidade/CadastroCidade");
+		mv.addObject("estados", estados.findAll());
+		return mv;
 	}
 	
 	@RequestMapping(consumes = MediaType.APPLICATION_JSON_VALUE)
@@ -29,5 +46,22 @@ public class CidadesController {
 			@RequestParam(name = "estado", defaultValue = "-1") Long codigoEstado) {
 
 		return cidades.findByEstadoCodigo(codigoEstado);
+	}
+	
+	@PostMapping("/nova")
+	public ModelAndView salvar(@Valid Cidade cidade, BindingResult result, RedirectAttributes attributes) {
+		if(result.hasErrors())
+			return nova(cidade);
+		
+		try {
+			cadastroCidadeService.salvar(cidade);
+			
+		} catch (NomeCidadeJaCadastradaException e) {
+			result.rejectValue("nome", e.getMessage(), e.getMessage());
+			return nova(cidade);
+		}
+		
+		attributes.addFlashAttribute("mensagem", "Cidade salva com sucesso!");
+		return new ModelAndView("redirect:/cidades/nova");
 	}
 }
--- src/main/java/com/danieldhsd/brewer/repository/Cidades.java
+++ src/main/java/com/danieldhsd/brewer/repository/Cidades.java
@@ -1,12 +1,16 @@
 package com.danieldhsd.brewer.repository;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.danieldhsd.brewer.model.Cidade;
+import com.danieldhsd.brewer.model.Estado;
 
 public interface Cidades extends JpaRepository<Cidade, Long> {
 	
 	public List<Cidade> findByEstadoCodigo(Long codigoEstado);
+	
+	public Optional<Cidade> findByNomeAndEstado(String nome, Estado estado);
 }
