709b9f6af55c5832d63d07426b17618b768f7957
Daniel Dutra <danieldhsd@gmail.com>
2021-04-04 14:42:23 -0300

    Configurando spring security no projeto

---
+++ src/main/java/com/danieldhsd/brewer/config/init/SecurityInitializer.java
@@ -0,0 +1,7 @@
+package com.danieldhsd.brewer.config.init;
+
+import org.springframework.security.web.context.AbstractSecurityWebApplicationInitializer;
+
+public class SecurityInitializer extends AbstractSecurityWebApplicationInitializer {
+
+}
