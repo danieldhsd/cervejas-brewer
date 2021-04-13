0f8d849ebf83783e85c08455d3ea829befb08f58
Daniel Dutra <danieldhsd@gmail.com>
2021-04-12 21:35:06 -0300

    Implementando filtro por grupo na pesquisa de usuarios

--- src/main/java/com/danieldhsd/brewer/repository/helper/usuario/UsuariosQueries.java
+++ src/main/java/com/danieldhsd/brewer/repository/helper/usuario/UsuariosQueries.java
@@ -4,9 +4,12 @@ import java.util.List;
 import java.util.Optional;
 
 import com.danieldhsd.brewer.model.Usuario;
+import com.danieldhsd.brewer.repository.filter.UsuarioFilter;
 
 public interface UsuariosQueries {
 	public Optional<Usuario> porEmailEAtivo(String email);
 	
 	public List<String> permissoes(Usuario usuario);
+	
+	public List<Usuario> filtrar(UsuarioFilter filtro);
 }
