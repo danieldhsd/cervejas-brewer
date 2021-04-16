f4a903500f3c76ecde20836cc9f14b0f84ed6126
Daniel Dutra <danieldhsd@gmail.com>
2021-04-15 22:24:56 -0300

    Renderizando cervejas com autocomplete

--- src/main/java/com/danieldhsd/brewer/dto/CervejaDTO.java
+++ src/main/java/com/danieldhsd/brewer/dto/CervejaDTO.java
@@ -2,6 +2,8 @@ package com.danieldhsd.brewer.dto;
 
 import java.math.BigDecimal;
 
+import org.springframework.util.StringUtils;
+
 import com.danieldhsd.brewer.enumeration.Origem;
 
 public class CervejaDTO {
@@ -19,7 +21,7 @@ public class CervejaDTO {
 		this.nome = nome;
 		this.origem = origem.getDescricao();
 		this.valor = valor;
-		this.foto = foto;
+		this.foto = StringUtils.isEmpty(foto) ? "cerveja-mock.png" : foto;
 	}
 
 	public Long getCodigo() {
--- src/main/resources/static/javascripts/brewer.js
+++ src/main/resources/static/javascripts/brewer.js
@@ -90,6 +90,11 @@ Brewer.Security = (function() {
 	
 }());
 
+Brewer.formatarMoeda = function(valor) {
+	numeral.language('pt-br');
+	return numeral(valor).format('0,0.00');
+}
+
 $(function() {
 	var maskMoney = new Brewer.MaskMoney();
 	maskMoney.enable();
@@ -105,4 +110,5 @@ $(function() {
 	
 	var security = new Brewer.Security();
 	security.enable();
+	
 });
