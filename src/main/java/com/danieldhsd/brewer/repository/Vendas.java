a56be0b58d28231eef332d7ef4b69e3bd8b8bd90
Daniel Dutra <danieldhsd@gmail.com>
2021-04-28 22:36:29 -0300

    Implementando a pesquisa de vendas

--- src/main/java/com/danieldhsd/brewer/repository/Vendas.java
+++ src/main/java/com/danieldhsd/brewer/repository/Vendas.java
@@ -3,7 +3,8 @@ package com.danieldhsd.brewer.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.danieldhsd.brewer.model.Venda;
+import com.danieldhsd.brewer.repository.helper.venda.VendasQueries;
 
-public interface Vendas extends JpaRepository<Venda, Long> {
+public interface Vendas extends JpaRepository<Venda, Long>, VendasQueries {
 
 }
---
+++ src/main/java/com/danieldhsd/brewer/repository/helper/venda/VendasQueries.java
@@ -0,0 +1,12 @@
+package com.danieldhsd.brewer.repository.helper.venda;
+
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+
+import com.danieldhsd.brewer.model.Venda;
+import com.danieldhsd.brewer.repository.filter.VendaFilter;
+
+public interface VendasQueries {
+
+	public Page<Venda> filtrar(VendaFilter filtro, Pageable pageable);
+}
